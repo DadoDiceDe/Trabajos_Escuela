e837e6358f9b910a367ff142d3a5f86b16218533
DadoDiceDe <90919482+DadoDiceDe@users.noreply.github.com>
2021-12-02 20:37:19 -0600

    Delete Tarea 5_Sistema Operativo_Diego.py

--- Tarea 5_Sistema Operativo_Diego.py
+++
@@ -1,7 +0,0 @@
-diccionario = {}
-diccionario = {"Estevan":[34,1.34], "Joaquin":[48,1.85], "Alexa":[22,1.32]}
-
-diccionario["Pedro"] = "88","1.59"
-del(diccionario["Alejandro"])
-
-print(diccionario)
